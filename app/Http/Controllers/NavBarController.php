b4539fb670e8c489e6940e9b9d14880c0cabc0c8
Aleksej-F <l7933@yandex.ru>
2022-04-28 09:59:48 +0300

    dz2 z1

---
+++ app/Http/Controllers/NavBarController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NavbarController extends Controller
+{
+    public function navBar()
+    {
+       
+       
+        return view('navbar');
+    }
+    
+   
+}
